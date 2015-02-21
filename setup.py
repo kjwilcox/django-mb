ee19b464db3518cbbc95f58d3956a758f38e854c
Kyle Wilcox <k.j.wilcox@gmail.com>
2015-02-21 15:17:39 -0800

    Renamed source directoy, added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-mb',
+    version='0.1',
+    description='A simple message board written in Django',
+    author='Kyle Wilcox',
+    author_email='k.j.wilcox@gmail.com',
+    install_requires=(
+        'Django',
+    )
+)
